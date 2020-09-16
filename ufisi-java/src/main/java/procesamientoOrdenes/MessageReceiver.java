1667bdb77a2d5212b952555a9e827612fc77fa0b
benja-123 <benjamin.calderon@unmsm.edu.pe>
2020-09-16 01:56:06 -0500

    nueva version

--- ufisi-java/src/main/java/procesamientoOrdenes/MessageReceiver.java
+++ ufisi-java/src/main/java/procesamientoOrdenes/MessageReceiver.java
@@ -9,30 +9,6 @@ import java.util.Properties;
 
 public class MessageReceiver {
 
-    /*static final String TOPIC = "test";
-    static final String GROUP = "test_group";
-
-    public static void main(String[] args){
-        Properties props = new Properties();
-        props.put("bootstrap.servers","localhost:9092");
-        props.put("group.id",GROUP);
-        props.put("auto.commit.interval.ms","1000");
-        props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-
-        try(KafkaConsumer<String,String> consumer = new KafkaConsumer<>(props);){
-            consumer.subscribe(Arrays.asList(TOPIC));
-            for(int i = 0; i<1000; i++){
-                ConsumerRecords<String,String> records = consumer.poll(1000L);
-                System.out.println("Size: "+records.count());
-                for(ConsumerRecord<String,String> record:records){
-                    System.out.println("Received a message: "+record.value());
-                }
-            }
-        }
-        System.out.println("End");
-    }*/
-
     public String recibir(String topico, String grupoId) {
         String mensaje = "";
         Properties props = new Properties();
