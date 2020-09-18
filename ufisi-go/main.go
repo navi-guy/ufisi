541dfe9868fe0db48cfd95e987182b24cc67bc1f
Aliaga <navichicken@gmail.com>
2020-09-18 15:49:48 -0500

    add model

--- ufisi-go/main.go
+++ ufisi-go/main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
-	appKafka "ufisi-go/consumer"
 	"ufisi-go/db"
+	"ufisi-go/model"
+	// "ufisi-go/model"
+	// "time"
+	// appKafka "ufisi-go/consumer"
 )
-
-type User struct {
-	IDFactura     int     `json:"id_factura"`
-	IDOrden       int     `json:"id_orden"`
-	CodigoFactura string  `json:"codigo_factura"`
-	Fecha         string  `json:"fecha"`
-	Total         float32 `json:"total"`
-}
 
 func main() {
 
-	db.CreateDatabase("facturacion")
-	fmt.Println("BD creada correctamente")
-	go appKafka.StartKafka()
-	fmt.Println("Kafka has been started...")
-	time.Sleep(10 * time.Minute)
+	//productos := "{\"products\": [{\"id\":\"21\",\"nombre\":\"papel\",\"precio\":20.5,\"cantidad\":0},{\"id\":\"22\",\"nombre\":\"madera\",\"precio\":10.0,\"cantidad\":0}]}"
+	jsonMsge := `{"codigo_factura": "F0008", "total_bruto": 12.5,"igv": 5.2, "fecha": "2020-09-16", "datox": "adsasda"}`
+	fact := model.Factura{}
+	json.Unmarshal([]byte(jsonMsge), &fact)
+	fmt.Println("Factura = ", fact)
+	idFactura := StoreFactura(fact)
+	fmt.Println(idFactura)
+	fmt.Println("Insertado!")
+	//db.CreateDatabase("facturacion_db")
+	//fmt.Println("BD facturacion_db creada correctamente")
+	//go appKafka.StartKafka()
+	//fmt.Println("Kafka has been started...")
+	//time.Sleep(10 * time.Minute)
+}
+
+// StoreFactura insert on database the facturas and item_factura
+func StoreFactura(f model.Factura) (id int64) {
+	db := db.Conn()
+	stmt, err := db.Prepare("INSERT into facturas(codigo_factura,total_bruto,igv,fecha) values(?,?,?,?)")
+	checkErr(err)
+	res, err := stmt.Exec(f.CodigoFactura, f.TotalBruto, f.Igv, f.Fecha)
+	checkErr(err)
+	id, err = res.LastInsertId()
+	checkErr(err)
+	return id
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ ufisi-go/model/factura.go
@@ -0,0 +1,10 @@
+package model
+
+// Factura is the struct for table 'facturas'
+type Factura struct { // exported struct type
+	IDFactura     int     `json:"id_factura"`
+	IDOrden       int     `json:"id_orden"`
+	CodigoFactura string  `json:"codigo_factura"`
+	TotalBruto    float32 `json:"total_bruto"`
+	Igv           float32 `json:"igv"`
+}
