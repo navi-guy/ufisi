bc73e5daa83d23d5d59fe329870008c62b98eb91
Jesus0508 <jesus.leo0508@gmail.com>
2020-09-19 10:03:19 -0500

    Cambio nombre de la variable a mensaje

--- ufisi-node/src/handlers/sendAccountReceivable.js
+++ ufisi-node/src/handlers/sendAccountReceivable.js
@@ -1,19 +1,20 @@
 const send = (producer, topicName, accountReceivable) => {
-    const message = {
-        estado: 0,
-        message: `Su pedido se ha registrado con exito!.\n La fecha del pago esta programada para el dia ${accountReceivable.paymentDay}`
-    }
+        console.log("Enviando Mensaje ...")
+        const message = {
+            estado: 1,
+            mensaje: `Su pedido se ha registrado con exito!.\n La fecha del pago esta programada para el dia ${accountReceivable.paymentDay}`
+        }
+        const data = [
+            { topic: topicName, messages: JSON.stringify(message) }
+        ];
+        console.log(data);
+        producer.send(data, function (err, resp) {
+            console.log(resp);
+        });
+        producer.on('error', function (err) {
+            console.log(err);
+        });
 
-    const data = [
-        { topic: topicName, messages: JSON.stringify(message) }
-    ];
-    console.log(data);
-    producer.send(data, function (err, resp) {
-        console.log(resp);
-    });
-    producer.on('error', function (err) {
-        console.log(err);
-    });
 }
 
 module.exports = send;
