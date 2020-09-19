1b5750ddc708e610eb3114c7e5763c431b9da289
Aliaga <navichicken@gmail.com>
2020-09-19 06:47:59 -0500

    golang send Message

--- ufisi-python/producer.py
+++ ufisi-python/producer.py
@@ -2,9 +2,11 @@
 from kafka import KafkaProducer
 import json
 
-def sendToFacturacion(msge_to_facturacion=""):
+def produce_message(topic="",msg=""):
     # Mi productor
     producerPy = KafkaProducer(bootstrap_servers =['ec2-3-85-41-237.compute-1.amazonaws.com:9092'],
                     value_serializer=lambda v: json.dumps(v).encode('utf-8'), 
                     api_version=(0,10))
-    producerPy.send("facturacion", msge_to_facturacion)
+    producerPy.send(topic, msg)
+
+
