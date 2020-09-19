c8b465320b9e3532502c8b8ae362278d7969dcbc
Jesus0508 <jesus.leo0508@gmail.com>
2020-09-19 08:21:28 -0500

    Imagenes y validaciones GUI

--- ufisi-java/src/main/java/procesamientoOrdenes/Producto.java
+++ ufisi-java/src/main/java/procesamientoOrdenes/Producto.java
@@ -1,11 +1,14 @@
 package procesamientoOrdenes;
 
+import javax.swing.*;
+
 public class Producto {
 
     private String codigo_producto;
     private String nombre;
     private double precio;
     private int cantidad;
+    private ImageIcon imagen;
 
     public Producto(String codigo_producto, String nombre, double precio, int cantidad) {
         this.codigo_producto = codigo_producto;
@@ -14,7 +17,8 @@ public class Producto {
         this.cantidad = cantidad;
     }
 
-    public Producto(String codigo_producto, String nombre, double precio) {
+    public Producto(String codigo_producto, String nombre, double precio, ImageIcon imagen) {
+        this.imagen = imagen;
         this.codigo_producto = codigo_producto;
         this.nombre = nombre;
         this.precio = precio;
@@ -52,7 +56,15 @@ public class Producto {
         this.cantidad = cantidad;
     }
 
+    public ImageIcon getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(ImageIcon imagen) {
+        this.imagen = imagen;
+    }
+
     public String[] convertirString() {
-        return new String[]{codigo_producto, nombre, Double.toString(precio),Integer.toString(cantidad)};
+        return new String[]{codigo_producto, nombre, Double.toString(precio), Integer.toString(cantidad)};
     }
 }
--- ufisi-java/src/main/java/procesamientoOrdenes/View.java
+++ ufisi-java/src/main/java/procesamientoOrdenes/View.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -14,8 +15,6 @@ public class View extends JFrame {
     private final MessageSender productor;
     private final MessageReceiver consumidor;
     private final ArrayList<Producto> productosCarrito;
-    private final Producto productoPrueba1 = new Producto("21", "papel", 20.5);
-    private final Producto productoPrueba2 = new Producto("22", "madera", 10);
     private JPanel panel1;
     private JTextField productoTextField;
     private JTextField cantidadTextField;
@@ -25,11 +24,15 @@ public class View extends JFrame {
     private JButton realizarPedidoButton;
     private JLabel labelMontoTotal;
     private JTable tableProductos;
+    private JPanel JPanelTabla;
+    private JScrollPane JScroll;
     private List<Producto> listaProductos;
     private double montoTotal = 0.0;
+    private JTable tablitaConImagen;
 
     public View() {
         super("Modulo Productos");
+        this.setLocationRelativeTo(null);
         setContentPane(panel1);
         productor = new MessageSender();
         consumidor = new MessageReceiver();
@@ -38,7 +41,7 @@ public class View extends JFrame {
 
         tableProductos.setModel(new DefaultTableModel(
                 null,
-                new String[]{"Codigo", "Nombre", "Precio (S/.)"}
+                new String[]{"Imagen", "Codigo", "Nombre", "Precio (S/.)"}
         ));
         tableProductosCarrito.setModel(new DefaultTableModel(
                 null,
@@ -67,35 +70,39 @@ public class View extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 DefaultTableModel model = (DefaultTableModel) tableProductosCarrito.getModel();
                 String nombre = productoTextField.getText();
-                int cantidad = Integer.parseInt(cantidadTextField.getText());
-                boolean encontrado = false;
-                int i = 0;
-                while (i < productosCarrito.size() && !encontrado) {
-                    if (productosCarrito.get(i).getNombre().equals(nombre)) {
-                        int nuevaCantidad = productosCarrito.get(i).getCantidad() + cantidad;
-                        montoTotal = montoTotal + productosCarrito.get(i).getPrecio() * cantidad;
-                        productosCarrito.get(i).setCantidad(nuevaCantidad);
-                        model.setValueAt(nuevaCantidad, i, 3);
-                        encontrado = true;
+                String cantidadStr = cantidadTextField.getText();
+                if (!cantidadStr.isEmpty() && cantidadStr.matches("^[1-9]\\d*$")) {
+                    int cantidad = Integer.parseInt(cantidadStr);
+                    boolean encontrado = false;
+                    int i = 0;
+                    while (i < productosCarrito.size() && !encontrado) {
+                        if (productosCarrito.get(i).getNombre().equals(nombre)) {
+                            int nuevaCantidad = productosCarrito.get(i).getCantidad() + cantidad;
+                            montoTotal = montoTotal + productosCarrito.get(i).getPrecio() * cantidad;
+                            productosCarrito.get(i).setCantidad(nuevaCantidad);
+                            model.setValueAt(nuevaCantidad, i, 3);
+                            encontrado = true;
+                        }
+                        i++;
                     }
-                    i++;
-                }
-                if (!encontrado) {
-                    for (Producto producto : listaProductos) {
-                        if (producto.getNombre().equals(nombre)) {
-                            Producto nuevoProducto = new Producto(producto.getCodigo_producto(), producto.getNombre(), producto.getPrecio(), cantidad);
-                            productosCarrito.add(nuevoProducto);
-                            model.addRow(nuevoProducto.convertirString());
-                            montoTotal = montoTotal + nuevoProducto.getPrecio() * nuevoProducto.getCantidad();
+                    if (!encontrado) {
+                        for (Producto producto : listaProductos) {
+                            if (producto.getNombre().equals(nombre)) {
+                                Producto nuevoProducto = new Producto(producto.getCodigo_producto(), producto.getNombre(), producto.getPrecio(), cantidad);
+                                productosCarrito.add(nuevoProducto);
+                                model.addRow(nuevoProducto.convertirString());
+                                montoTotal = montoTotal + nuevoProducto.getPrecio() * nuevoProducto.getCantidad();
 
+                            }
                         }
                     }
+                    montoTotal = Math.round(montoTotal * 100.00) / 100.00;
+                    labelMontoTotal.setText(Double.toString(montoTotal));
+                } else {
+                    JOptionPane.showMessageDialog(null,"Ingrese una cantidad valida", "Alerta", JOptionPane.WARNING_MESSAGE);
                 }
-                montoTotal = Math.round(montoTotal * 100.00) / 100.00;
-                labelMontoTotal.setText(Double.toString(montoTotal));
             }
         });
-
         quitarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -110,7 +117,6 @@ public class View extends JFrame {
                             montoTotal = montoTotal - productosCarrito.get(i).getPrecio() * productosCarrito.get(i).getCantidad();
                             productosCarrito.remove(productosCarrito.get(i));
                             model.removeRow(fila);
-                            labelMontoTotal.setText(Double.toString(montoTotal));
                             encontrado = true;
                         }
                         i++;
@@ -126,31 +132,75 @@ public class View extends JFrame {
                             montoTotal = montoTotal - productosCarrito.get(i).getPrecio() * cantidad;
                             productosCarrito.get(i).setCantidad(nuevaCantidad);
                             model.setValueAt(nuevaCantidad, i, 3);
-                            labelMontoTotal.setText(Double.toString(montoTotal));
                             encontrado = true;
                         }
                         i++;
                     }
                 }
+                montoTotal = Math.round(montoTotal * 100.00) / 100.00;
+                labelMontoTotal.setText(Double.toString(montoTotal));
+            }
+        });
+        tablitaConImagen.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                int row = tablitaConImagen.rowAtPoint(evt.getPoint());
+                int col = tablitaConImagen.columnAtPoint(evt.getPoint());
+                if (row >= 50 && col >= 4) {
+                    System.out.println("Seleccione un a fila correcta");
+                } else {
+                    productoTextField.setText(listaProductos.get(row).getNombre());
+                    cantidadTextField.setText("0");
 
+                }
+            }
+        });
+        tableProductosCarrito.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                int row = tableProductosCarrito.rowAtPoint(evt.getPoint());
+                int col = tableProductosCarrito.columnAtPoint(evt.getPoint());
+                if (row >= 50 && col >= 4) {
+                    System.out.println("Seleccione un a fila correcta");
+                } else {
+                    productoTextField.setText(productosCarrito.get(row).getNombre());
+                    cantidadTextField.setText("0");
+                }
             }
         });
     }
 
     private void iniciarTablaProductos() {
-        DefaultTableModel model = (DefaultTableModel) tableProductos.getModel();
-        listaProductos = Arrays.asList(new Producto("1", "silicona", 3.10),
-                new Producto("2", "borrador", 1.80),
-                new Producto("3", "tijera", 2.10),
-                new Producto("4", "boligrafo", 2.90),
-                new Producto("5", "folder", 4.80),
-                new Producto("6", "lapicero", 4.10),
-                new Producto("7", "resaltador", 4.20),
-                new Producto("8", "compas", 5.20),
-                new Producto("9", "tajador", 1.00));
-        for (Producto producto : listaProductos) {
-            model.addRow(producto.convertirString());
+        DefaultTableModel model = new DefaultTableModel() {
+            @Override
+            public Class getColumnClass(int column) {
+                if (column == 0) return ImageIcon.class;
+                return Object.class;
+            }
+        };
+        model.setColumnIdentifiers(new String[]{"Imagen", "Codigo", "Nombre", "Precio (S/.)"});
+        listaProductos = Arrays.asList(new Producto("1", "Silicona", 3.10, new ImageIcon(getClass().getResource("/images/glue.png"))),
+                new Producto("2", "Borrador", 1.80, new ImageIcon(getClass().getResource("/images/eraser.png"))),
+                new Producto("3", "Tijera", 2.10, new ImageIcon(getClass().getResource("/images/scissors.png"))),
+                new Producto("4", "Boligrafo", 2.90, new ImageIcon(getClass().getResource("/images/fountain-pen.png"))),
+                new Producto("5", "Folder", 4.80, new ImageIcon(getClass().getResource("/images/folder.png"))),
+                new Producto("6", "Lapicero", 4.10, new ImageIcon(getClass().getResource("/images/pen.png"))),
+                new Producto("7", "Resaltador", 4.20, new ImageIcon(getClass().getResource("/images/marker.png"))),
+                new Producto("8", "Compas", 5.20, new ImageIcon(getClass().getResource("/images/compass.png"))),
+                new Producto("9", "Tajador", 1.00, new ImageIcon(getClass().getResource("/images/pencil-sharpener.png"))));
+        int i = 0;
+        for (Producto productoI : listaProductos) {
+            model.addRow(new String[]{"Imagen Referencial", productoI.getCodigo_producto(), productoI.getNombre(), Double.toString(productoI.getPrecio())});
+            model.setValueAt(productoI.getImagen(), i, 0);
+            i++;
         }
+        this.tablitaConImagen = new JTable(model);
+        this.tablitaConImagen.setPreferredScrollableViewportSize(this.JScroll.getPreferredSize());
+        JScrollPane scrollPane = new JScrollPane(this.tablitaConImagen);
+        this.tablitaConImagen.setRowHeight(72);
+        JPanelTabla.setLayout(new GridBagLayout());
+        JPanelTabla.remove(this.JScroll);
+        JPanelTabla.add(scrollPane);
     }
 
 
